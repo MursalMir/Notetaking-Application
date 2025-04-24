b0b82b189e43ee01d62bf57a2186d4b4ece0494b
Tyrell <Rellsteration@gmail.com>
2025-04-25 01:26:08 +1000

    Added upload functionality, parsing of uploaded files and database insertion for uploaded files

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,10 @@ module com.example.notetaker {
     requires javafx.fxml;
     requires java.sql;
 
+    // Required for file parsing libraries (model/FileParser.java)
+    requires org.apache.pdfbox;
+    requires org.apache.poi.ooxml;
+
     opens com.example.notetaker.controller to javafx.fxml;
     exports com.example.notetaker;
 }
