23ea8f4aed3e00ef7003f69c26b53ba75e312e0c
Rua-dri <alokenian12@gmail.com>
2025-04-28 20:49:40 +1000

    Merged all of our branches with main. Moved AI functionality from the placeholder window into the AI view

--- src/main/java/com/example/notetaker/controller/AIViewController.java
+++ src/main/java/com/example/notetaker/controller/AIViewController.java
@@ -1,29 +1,50 @@
 package com.example.notetaker.controller;
 
+import com.example.notetaker.service.OllamaConnection;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
-import javafx.scene.control.Label;
 
 import java.io.IOException;
 
 public class AIViewController {
 
-    // Placeholder for future AI logic
+
     @FXML
-    private Label placeholderLabel;
+    private Button backToDashboardButton;
+
+    @FXML
+    private TextArea noteEntryArea;
+
+    @FXML
+    private TextArea expandedNoteArea;
+
+    OllamaConnection ollamaConnection = new OllamaConnection();
 
     @FXML
     public void returnToHome() {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/notetaker/MainView.fxml"));
-            Stage stage = (Stage) placeholderLabel.getScene().getWindow();
+            Stage stage = (Stage) backToDashboardButton.getScene().getWindow();
             stage.setScene(new Scene(loader.load()));
             stage.setTitle("Dashboard");
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     Event handler for the generate button
+     */
+    @FXML
+    private void onGenerateButtonClick(ActionEvent event) {
+        // Generate improved notes from the ones in the noteEntryArea and print them in the
+        // expanded note area.
+        expandedNoteArea.setText(ollamaConnection.generateExpandedNotes(noteEntryArea.getText()));
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,14 @@
 module com.example.notetaker {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.net.http;
     requires java.sql;
-
+    requires com.google.gson;
+    opens com.example.notetaker to javafx.fxml;
     opens com.example.notetaker.controller to javafx.fxml;
     exports com.example.notetaker;
+    exports com.example.notetaker.controller;
+    exports com.example.notetaker.service;
+    exports com.example.notetaker.model;
+
 }
